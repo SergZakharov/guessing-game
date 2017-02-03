2d5e19b93bb66a460bf9d3d38bc2967c4d8c8999
SergZakharov <z_sergey@tut.by>
2017-02-03 03:03:21 +0300

    Final version

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor(min,max,mid) {
+      this.min; //hope it ok here?
+      this.max;
+      this.mid;
+
+    }
 
     setRange(min, max) {
-
+      this.min=min+1; // set up range
+      this.max=max;   // which is len - 1
     }
 
     guess() {
-
+    return this.mid=Math.floor((this.min+this.max)/2);
+    //the position of the middle element as in optimizied BSA
     }
 
     lower() {
-
+    this.max=this.mid;//set up R margin if lower
     }
-
     greater() {
-
+    this.min=this.mid+1; //set up l margin greater
     }
 }
 
